0693004a08ee881e14acf8e9b92dc952fdef5b4b
Nyx666666 <159556083+Nyx666666@users.noreply.github.com>
2024-10-08 14:52:41 +0100

    should work(nope)

--- netconect3/Program.cs
+++ netconect3/Program.cs
@@ -1,40 +1,79 @@
-﻿
-
-
-
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+//using System.Windows.Forms;
 
+
+
+
+sockets t1;
+
+t1 = new sockets(5200);
+
+sockets t2;
+
+t2 = new sockets(Dns.GetHostAddresses(Dns.GetHostName())[0], 5200);
 class sockets
 {
-    sockets(IPAddress iptarg, UInt16 port)
+    public sockets(IPAddress iptarg, UInt16 port)
     {
         IPEndPoint hostendpoint = new(Dns.GetHostAddresses(Dns.GetHostName())[0], port);
-        Socket conport = new Socket(hostendpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        Socket openport = new Socket(hostendpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        Console.WriteLine("connetcting");
 
         long T = DateTime.Now.Ticks;
-        while (!conport.Connected)
+        
+        while (true)
         {
-            conport.ConnectAsync(new IPEndPoint(iptarg, port));
-
-
-            if (T > DateTime.Now.Ticks + 5000)//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH REMOVE LATER
+            while (!openport.Connected)
             {
-                try
+                openport.ConnectAsync(new IPEndPoint(iptarg, port));
+
+                // Console.WriteLine((DateTime.Now.Ticks-T));
+
+
+                if (T < DateTime.Now.Ticks - 50000000)//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH REMOVE LATER
                 {
-                    new Exception("time out connecting");
+                    Console.WriteLine("time out connecting");
+                    return;
+
                 }
-                catch(Exception e) { Debug.WriteLine(e.Message); }
+
+            }
+            
+            openport.Send(Encoding.UTF8.GetBytes("<CONSTART>"));
+            byte[] buffer = new byte[1024];
+
+
+            byte[] awaitmsg = Encoding.UTF8.GetBytes("<CONSTART>");
+            while (true)
+            {
+                if (openport.Receive(buffer, SocketFlags.None).ToString() == awaitmsg.ToString())
+                {
+                    Console.WriteLine("<CONSTART> receved");
+                    openport.Send(Encoding.UTF8.GetBytes("<ACK>"));
+                    break;
+                }
+                if (T < DateTime.Now.Ticks - 50000000)//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH REMOVE LATER
+                {
+                    Console.WriteLine("time out connecting");
+                    return;
+
+                }
             }
         }
+        Console.WriteLine("conected");
     }
-    sockets(UInt16 port)//listen port
+    public sockets(UInt16 port)//listen port
     {
+        IPEndPoint hostendpoint = new(Dns.GetHostAddresses(Dns.GetHostName())[0], port);
+        Socket listen = new Socket(hostendpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
         try
         {
-            IPEndPoint hostendpoint = new(Dns.GetHostAddresses(Dns.GetHostName())[0], port);
-            Socket listen = new Socket(hostendpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            
 
             listen.Bind(hostendpoint);
             listen.Listen(port);
@@ -46,6 +85,18 @@ class sockets
         catch(Exception e)
         {
             Debug.WriteLine(e.ToString());
+        }
+        while (!listen.Connected) { }
+        byte[] buffer = new byte[1024];
+        byte[] awaitmsg = Encoding.UTF8.GetBytes("<CONSTART>");
+        while (true)
+        {
+            if (listen.Receive(buffer, SocketFlags.None).ToString() == awaitmsg.ToString())
+            {
+                Console.WriteLine("<CONSTART> receved");
+                listen.Send(Encoding.UTF8.GetBytes("<ACK>"));
+                break;
+            }
         }
     }
 
