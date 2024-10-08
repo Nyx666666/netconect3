ca946ddaf607f348299021a53fe26d573e8651af
Nyx666666 <159556083+Nyx666666@users.noreply.github.com>
2024-10-08 12:39:33 +0100

    created listen port, stared on connecting port

--- netconect3/Program.cs
+++ netconect3/Program.cs
@@ -1,1 +1,53 @@
-﻿aa
+﻿
+
+
+
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+class sockets
+{
+    sockets(IPAddress iptarg, UInt16 port)
+    {
+        IPEndPoint hostendpoint = new(Dns.GetHostAddresses(Dns.GetHostName())[0], port);
+        Socket conport = new Socket(hostendpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        long T = DateTime.Now.Ticks;
+        while (!conport.Connected)
+        {
+            conport.ConnectAsync(new IPEndPoint(iptarg, port));
+
+
+            if (T > DateTime.Now.Ticks + 5000)//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH REMOVE LATER
+            {
+                try
+                {
+                    new Exception("time out connecting");
+                }
+                catch(Exception e) { Debug.WriteLine(e.Message); }
+            }
+        }
+    }
+    sockets(UInt16 port)//listen port
+    {
+        try
+        {
+            IPEndPoint hostendpoint = new(Dns.GetHostAddresses(Dns.GetHostName())[0], port);
+            Socket listen = new Socket(hostendpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            listen.Bind(hostendpoint);
+            listen.Listen(port);
+        }
+        catch(SocketException e)
+        {
+            Debug.WriteLine(e.ToString());
+        }
+        catch(Exception e)
+        {
+            Debug.WriteLine(e.ToString());
+        }
+    }
+
+
+}
