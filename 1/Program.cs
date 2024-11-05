4bc6eab071a13e46f2a4ca95f9518d00d6fcfa5c
Nyx666666 <159556083+Nyx666666@users.noreply.github.com>
2024-11-05 11:51:27 +0000

    initial
    moved netconnect3 into a winforms app

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+using sockets;
+
+namespace _1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
